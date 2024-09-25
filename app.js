12e3966aaa6c7dac7f67484c3994d8b94fd946e8
XislatRaximjonov <yapon11xislat@gmail.com>
2024-09-25 15:10:10 +0500

    swager

--- app.js
+++ app.js
@@ -4,8 +4,18 @@ import productsRoutes from "./routes/productsRoutes.js";
 import usersRoutes from "./routes/usersRoutes.js";
 import swaggerJSDoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
 
 const app = express();
+
+
+const __filname = fileUrlToPath(import.meta.url);
+const __dirname = path.dirname(__filname);
+
+
+
 
 const options = {
 definition: {
@@ -14,9 +24,9 @@ definition: {
     title:"Ipadrom" ,
     version: "1.0.0",
     },
-    servers: [{url: "https://tes-work.onrender.com/"}]
+    servers: [{url: "http://localhost:10000"}]
     },
-    apis: [`${import.meta.dirname}./routes/*.js`]
+    apis: [`${join(__dirname , "routes", "*.js")}`],
 }
 
 const swaggerSpec = swaggerJSDoc(options)
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -2,6 +2,20 @@ import express from "express";
 import * as authController from "../Controllers/authController.js"
 const router = express.Router()
 
+
+/**
+ * @swagger
+ *
+ * /signup:
+ *  get:
+ *      summary: Welcome to server
+ *      description: welcome
+ *      responses:
+ *          200:
+ *              description: hello
+ *
+ */
+
 router.route("/signup").post(authController.signUp)
 router.route("/login").post(authController.signIn)
 
